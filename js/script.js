89095c63ca11ab20c104b16712cddc266a13745e
Fadl Al Obaid <149519983+fadlalobaid@users.noreply.github.com>
2025-04-16 19:19:38 +0300

    git add.

--- js/script.js
+++ js/script.js
@@ -1,32 +1,35 @@
-const $=(selector)=>{
-    return document.querySelector(selector)
+const $ = (selector) => {
+  return document.querySelector(selector);
+};
+const hour = $(".hour");
+const dot = $(".dot");
+const min = $(".min");
+const week = $(".week");
+
+let showDot = true;
+function update() {
+  showDot = !showDot;
+  const now = new Date();
+
+  if (showDot) {
+    dot.classList.add("invsible");
+  } else {
+    dot.classList.remove("invsible");
+  }
+  function nowHours() {
+    let hours = now.getHours();
+    const period = hours >= 12 ? "PM" : "AM";
+    let hours12 = hours % 12;
+    hours12 = hours12 || 12;
+    return hours12
+  }
+
+  hour.textContent = String(nowHours()).padStart(1, "12");
+  min.textContent = String(now.getMinutes()).padStart(2, "0");
+
+  Array.from(week.children).forEach((ele) => {
+    ele.classList.remove("active");
+  });
+  week.children[now.getDay()].classList.add("active");
 }
-const hour=$('.hour')
-const dot=$('.dot')
-const min=$('.min')
-const week=$('.week')
-
-let showDot=true
-function update(){
-    showDot= !showDot;
-    const now=new Date()
-
-    if(showDot){
-        dot.classList.add('invsible')
-    }else{
-        dot.classList.remove('invsible')
-    }
-    hour.textContent=String(now.getHours())
-    .padStart(1,'12')
-    min.textContent=String(now.getMinutes())
-    .padStart(2,'0')
-
-    Array .from(week.children)
-    .forEach(
-        (ele)=>{
-            ele.classList.remove('active')
-        }
-    );
-    week.children[now.getDay()].classList.add('active')
-}
-setInterval(update,500)
+setInterval(update, 500);
